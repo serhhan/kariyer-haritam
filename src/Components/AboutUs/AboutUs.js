c20b053519bc4169841ff14517eccc74603e95f2
serhanelmali <serhanelmali@gmail.com>
2021-04-01 02:34:01 +0300

    about us page styling changes

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -11,14 +11,53 @@ const AboutUs = () => {
         <div className="aboutUsContainer">
             <h1>BİZ KİMİZ?</h1>
             <div className="personCards">
-                <div className="Card"><div className="upperContainer"><img src ={cardPhoto} alt="Serhan Elmalı" /><span className = "name">Serhan Elmalı</span> <span className="title">FrontEnd Developer</span></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque. </div>
-                <div className="Card"><div className="upperContainer"><img src ={cardPhoto} alt="Serhan Elmalı" /><span className = "name">Serhan Elmalı</span> <span className="title">FrontEnd Developer</span></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque. </div>
-                <div className="Card"><div className="upperContainer"><img src ={cardPhoto} alt="Serhan Elmalı" /><span className = "name">Serhan Elmalı</span> <span className="title">FrontEnd Developer</span></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque. </div>
-                <div className="Card"><div className="upperContainer"><img src ={cardPhoto} alt="Serhan Elmalı" /><span className = "name">Serhan Elmalı</span> <span className="title">FrontEnd Developer</span></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque. </div>
-                <div className="Card"><div className="upperContainer"><img src ={cardPhoto} alt="Serhan Elmalı" /><span className = "name">Serhan Elmalı</span> <span className="title">FrontEnd Developer</span></div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque. </div>
+                <div className="Card">
+                    <div className="upperContainer">
+                        <img src ={cardPhoto} alt="Serhan Elmalı" />
+                        <div className="text-container">
+                            <span className = "name">Serhan Elmalı</span>
+                            <span className="title">FrontEnd Developer</span>
+                        </div>
+                    </div>
+                    <div className="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque.</div> </div>
+                <div className="Card">
+                    <div className="upperContainer">
+                        <img src ={cardPhoto} alt="Serhan Elmalı" />
+                        <div className="text-container">
+                            <span className = "name">Serhan Elmalı</span>
+                            <span className="title">FrontEnd Developer</span>
+                        </div>
+                    </div>
+                    <div className="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque.</div> </div>
+                <div className="Card">
+                    <div className="upperContainer">
+                        <img src ={cardPhoto} alt="Serhan Elmalı" />
+                        <div className="text-container">
+                            <span className = "name">Serhan Elmalı</span>
+                            <span className="title">FrontEnd Developer</span>
+                        </div>
+                    </div>
+                    <div className="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque.</div> </div>
+                <div className="Card">
+                    <div className="upperContainer">
+                        <img src ={cardPhoto} alt="Serhan Elmalı" />
+                        <div className="text-container">
+                            <span className = "name">Serhan Elmalı</span>
+                            <span className="title">FrontEnd Developer</span>
+                        </div>
+                    </div>
+                    <div className="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque.</div> </div>
+                <div className="Card">
+                    <div className="upperContainer">
+                        <img src ={cardPhoto} alt="Serhan Elmalı" />
+                        <div className="text-container">
+                            <span className = "name">Serhan Elmalı</span>
+                            <span className="title">FrontEnd Developer</span>
+                        </div>
+                    </div>
+                    <div className="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit leo, efficitur cursus lectus vel, aliquet congue orci. Aliquam iaculis augue condimentum nulla congue dignissim. Donec sed orci eu dolor facilisis posuere in sed elit. Fusce blandit mattis sollicitudin. Donec maximus purus turpis, et ullamcorper eros luctus ut. Phasellus maximus sapien eget quam laoreet, eu iaculis ligula ultrices. In eu finibus turpis, eu aliquam neque.</div> </div>
             </div>
         </div>
     )
 }
-
 export default AboutUs;
